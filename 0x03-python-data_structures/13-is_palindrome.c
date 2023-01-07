867b71a3fc3857ea6e6a0a76c9ac079febcb5f5f
obidigold <obidigold@gmail.com>
2023-01-07 05:02:38 -0800

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,72 +9,51 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *start = NULL, *end = NULL
-		unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
-
-	if (head == NULL)
-		return (0);
+	listint_t *a = *head;
+	listint_t *b = *head;
 
 	if (*head == NULL)
 		return (1);
 
-	start = *head;
-	len = listint_len(start);
-	len_cyc = len * 2;
-	len_list = len_cyc - 2;
-	end = *head;
-
-	for (; i < len_cyc; i = i + 2)
+	while (b && b->next && b->next->next)
 	{
-		if (start[i].n != end[len_list].n)
-			return (0);
+		a = a->next;
+		b = b->next->next;
+	}
 
-		len_list = len_list - 2;
+	a = reverse_list(&a);
+	b = *head;
+	while (a && b)
+	{
+		if (a->n != b->n)
+			return (0);
+		a = a->next;
+		b = b->next;
 	}
 
 	return (1);
 }
 
 /**
- * get_nodeint_at_index - Gets a node from a linked list
- * @head: The head of the linked list
- * @index: The index to find in the linked list
- * Return: The specific node of the linked list
+ * reverse_list - Reverse a linked list
+ * @head: The list
+ * Return: Pointer to the new head
+ * listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *reverse_list(listint_t **head)
 {
-	listint_t *current = head;
-	unsigned int iter_times = 0;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	if (head)
+	while (*head)
 	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
-
-			current = current->next;
-			++iter_times;
-		}
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	return (NULL);
+	*head = prev;
+	return (*head);
 }
 
-/**
- * slistint_len - Counts the number of elements in a linked list
- * @h: The linked list to count
- * Return: Number of elements in the linked list
- */
-size_t listint_len(const listint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-
-	return (lenght);
-}
