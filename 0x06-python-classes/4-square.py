4f4f99c94e16a716694adca445ab08d57d0779a3
obidigold <obidigold@gmail.com>
2023-01-25 02:58:35 -0800

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,51 +1,25 @@
 #!/usr/bin/python3
 class Square:
-    """Square Class
-
-    A Square Class
-
-    """
-
     def __init__(self, size=0):
-        """__init__
-
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-            Raises:
-                TypeError: If `size` type is not `int`.
-
-                ValueError: If `size` is less than `0`.
-
-            """
-
-            if type(size) is not int:
-                raise TypeError('size must be an integer')
-
-
-            if size < 0:
-                raise ValueError('size must be >= 0')
-
-            self.size = size
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
 
         @property
         def size(self):
             return self.__size
 
         @size.setter
-        def size(self, size):
-            if type(size) is not int:
-                raise TypeError('size must be an integer')
+        def size(self, value):
+            if isinstance(value, int) and value >= 0:
+                self.__size = value
+            elif (not isinstance(value, int)):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
 
-            if size < 0:
-                raise ValueError('size must be >= 0')
-
-            self.__size = size
-
-        def area(self):
-            """Returns the current square area
-
-            """
-            return self.__size ** 2
+            def area(self):
+                return (self.__size * self.__size)
