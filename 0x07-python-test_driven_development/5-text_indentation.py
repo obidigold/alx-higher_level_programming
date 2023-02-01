ea65b1e49b0b6186ca921febf5def18b8483c588
obidigold <obidigold@gmail.com>
2023-02-01 03:52:53 -0800

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,46 +1,25 @@
 #!/usr/bin/python3
 """
-Function that prints a text with 2 new lines after each of these characters:
-. ? and :
+This is the "5-test_indentation" module.
 
-    """
+The 5-text_indentation module supplies one function, text_indentation(text).
+"""
 
-    def text_indentation(text):
-        """
-        Prints a text replace some characters with 2 newlines
-
-        Arguments:
-
-        text: must be a string
-
-        If not a string a TypeError is raised
-
-        There is no space at the beginning at the end of each printed line
-
-        """
-
-        if not isinstance(text, str):
-            msg = "text must be a string"
-            raise TypeError(msg)
-
-        if len(text) == 0:
-            print(end='')
-            return None
-
-        textlist = []
-        mark = 0
-
-        for i in range(len(text)):
-            if text[i] == '.' or text[i] == '?' or text[i] == ':':
-                textlist.append(text[mark: i + 1] + "\n\n")
-                mark = i + 1
-
-        textlist.append(text[mark:i + 1])
-
-        textlist = [i.strip(' ') for i in textlist]
-
-        new_text = "".join(textlist)
-
-        print(new_text, end='')
-
-
+def text_indentation(text):
+    """splits a text into lines along "?", ":", "." followed by 2 new lines"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for a in text:
+        if flag == 0:
+            if a == ' ':
+                continue
+            else:
+                flag = 1
+                if flag == 1:
+                    if a == '?' or a == '.' or a == ':':
+                        print(a)
+                        print()
+                        flag = 0
+                    else:
+                        print(a, end="")
