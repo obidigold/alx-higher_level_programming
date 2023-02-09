8eb2235a01bebd11a9165f8e93b68ddb39ab2b1a
obidigold <obidigold@gmail.com>
2023-02-09 06:55:23 -0800

    function that writes a string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+""" Module that contains a function that writes to a text file
+"""
+
 
 
 def write_file(filename="", text=""):
-    """ function that writes a string to a text file (UTF8) and returns
-    the number of characters written"""
+    """ Function that writes to a text file
 
-    with open(filename, "w", encoding="utf-8") as myfile:
-        return myfile.write(text)
+    Args:
+    filename: filename
+    text: text to write
+
+    Raises
+    Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
